47ed0fdc24f2808b9964ddb0ccb8d2335a7cb404
Tim <c-n@TimdeMacBook-Pro.local>
2020-07-19 21:44:14 +0800

    works

--- js/update-alert.js
+++ js/update-alert.js
@@ -1,12 +1,6 @@
 $(document).ready(function () {
     //更新中提示視窗
     $('.update-alert').click(function (e) { 
-        $('.updateing').fadeIn(1000);
-        $('body').css({'overflow-y': 'hidden'});
         return false;
-    });
-    $('body').click(function (e) { 
-        $('.updateing').fadeOut(1000);
-        $('body').css({'overflow-y': 'auto'});
     });
 });
