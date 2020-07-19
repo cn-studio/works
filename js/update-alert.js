9294b27acd449b2faeefcb21c189654e25def092
Tim <c-n@TimdeMacBook-Pro.local>
2020-07-19 21:37:56 +0800

    works

--- js/update-alert.js
+++ js/update-alert.js
@@ -1,19 +1,12 @@
 $(document).ready(function () {
     //更新中提示視窗
-    $('.update-alert').on('click', function () {
-        $('body').append('<div class="updateing"></div>').css({'overflow-y':'hidden'});
-        $('.updateing').append('<div class="updateing-alert"></div>');
-        $('.updateing-alert').append('<p>哈哈，還再更新啦<br>是有沒有這麼期待</p><span>點擊任一處關閉</span>');
-        setTimeout(() => {
-            $('.updateing').fadeIn(1000);
-        }, 0);
+    $('.update-alert').click(function (e) { 
+        $('.updateing').fadeIn(1000);
+        $('body').css({'overflow-y': 'hidden'});
         return false;
     });
-    $('body').on('click','.updateing', function () { 
+    $('body').click(function (e) { 
         $('.updateing').fadeOut(1000);
-        setTimeout(() => {
-            $('body').css({'overflow-y':'auto'});
-            $('.updateing').remove();
-        }, 1000);
+        $('body').css({'overflow-y': 'auto'});
     });
 });
